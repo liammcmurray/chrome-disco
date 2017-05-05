e6003f4758186c5fbf07bd64d45ec77c33c997c7
Liam McMurray <liammcmurray@gmail.com>
2017-05-05 17:13:52 +0100

    added a check so that it only works for valid input

--- popup.js
+++ popup.js
@@ -5,6 +5,11 @@ document.getElementById("badURL").onkeydown= function (e) {
     outboundLink = document.getElementsByName("badURL")[0].value;;
     encodedLink = outboundLink.split('&URL=')[1];
     realLink = decodeURIComponent(encodedLink);
+    if (realLink === "undefined") {
+      console.log ('Not a valid URL, please check your clipboard');
+    }
+      else {
     chrome.tabs.create({ url: realLink });
+    }
   }
 }
